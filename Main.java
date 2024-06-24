6c84b75781d94493cda0db84954d733da6d7c752
Aastha Shah <63895874+tulip22@users.noreply.github.com>
2024-06-24 10:51:40 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,35 +1,64 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
+
     public static void main(String[] args) {
         btree tree = new btree(3);
 
+        // Declaring the ArrayList
+        List<studentRecord> arr = Collections.synchronizedList(new ArrayList<>(Collections.nCopies(100, null)));
         long begin = System.currentTimeMillis();
 
-
         // retrieval
-
         // creating executor service with cached thread pool
         ExecutorService serv = Executors.newCachedThreadPool();
         for(int i = 0; i < 100; i++){
             
             int num = i;
             serv.submit(() -> {
-                tree.retrieve(num);
+                //writing a function to fetch n store , then print in sorted order
+                //create arrayList and pass it in fetch , after all threads are done, 
+                studentRecord s = tree.retrieve(num);
+                synchronized(arr){
+                    if(s!=null){
+                        //System.out.println(s.rollNo);
+                        arr.add(s.rollNo, s);
+                        System.out.println((arr.get(s.rollNo)).toString());
+                    }
+                    else{
+                        arr.add(num, null);
+                    }
+                }
+                
             });
         }
 
         serv.shutdown();
 
         try{
-            serv.awaitTermination(60, TimeUnit.SECONDS);
+            serv.awaitTermination(300, TimeUnit.SECONDS);
             serv.shutdownNow();
         }
         catch(InterruptedException e){
             serv.shutdownNow();
         }
+
+        //printing records in sorted order
+        arr.forEach((s) ->{
+            if(s!= null){
+                System.out.println(s.toString());
+            }
+            else{
+                System.out.println("Roll no: " + s.rollNo + "Name: NA");
+            }
+           
+        }); 
+
         long end = System.currentTimeMillis();
 
         long time = end-begin;
